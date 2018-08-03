4afea22989152d1cf7fd062163cda78b995cec6f
groovypark <soobin950@gmail.com>
2018-08-03 18:39:05 +0900

    Update: firebase hosting

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomeView from '@/components/HomeView'
+import PostView from '@/components/PostView'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HomeView',
       component: HomeView
+    },
+    {
+      path: '/post',
+      name: 'PostView',
+      component: PostView
     }
   ]
 })
---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+var config = {
+  apiKey: 'AIzaSyBN6vDbvspkCmmZs1XhQz84KSDb4kGOu7s',
+  authDomain: 'photo-cloud-service.firebaseapp.com',
+  databaseURL: 'https://photo-cloud-service.firebaseio.com',
+  projectId: 'photo-cloud-service',
+  storageBucket: 'photo-cloud-service.appspot.com',
+  messagingSenderId: '310429767708'
+}
+firebase.initializeApp(config)
+
+export default {
+  database: firebase.database()
+}
