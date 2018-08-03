b0bcc3394a06deb519b214cacb0915d0c0045fbb
groovypark <soobin950@gmail.com>
2018-08-03 19:18:12 +0900

    Delete: api key

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase'
 
 var config = {
-  apiKey: 'AIzaSyBN6vDbvspkCmmZs1XhQz84KSDb4kGOu7s',
+  apiKey: '',
   authDomain: 'photo-cloud-service.firebaseapp.com',
   databaseURL: 'https://photo-cloud-service.firebaseio.com',
   projectId: 'photo-cloud-service',
