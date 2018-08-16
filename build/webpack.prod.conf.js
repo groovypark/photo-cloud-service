4c9de81b88635b76fc01b3b3f6b21d8deb96ad1e
groovypark <soobin950@gmail.com>
2018-08-16 16:15:14 +0900

    Update: runtimeCaching

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -110,17 +110,25 @@ const webpackConfig = merge(baseWebpackConfig, {
       minify: true,
       stripPrefix: 'dist/',
       runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/thecatapi\.com\/api\/images\/get\.php\?id/,
-          handler: 'cacheFirst'
-        }, {
-          urlPattern: /^https:\/\/(\d+)\.media\.tumblr\.com\//,
-          handler: 'cacheFirst'
-        }, {
-          urlPattern: /^http:\/\/(\d+)\.media\.tumblr\.com\//,
-          handler: 'cacheFirst'
-        },
-      ]
+      {
+        urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+        handler: 'cacheFirst'
+      }, {
+        urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+        handler: 'cacheFirst'
+      }, {
+        urlPattern: /^https:\/\/code\.getmdl\.io\//,
+        handler: 'cacheFirst'
+      }, {
+        urlPattern: /^https:\/\/thecatapi\.com\/api\/images\/get\.php\?id/,
+        handler: 'cacheFirst'
+      }, {
+        urlPattern: /^https:\/\/(\d+)\.media\.tumblr\.com\//,
+        handler: 'cacheFirst'
+      }, {
+        urlPattern: /^http:\/\/(\d+)\.media\.tumblr\.com\//,
+        handler: 'cacheFirst'
+      }]
     })
   ]
 })
